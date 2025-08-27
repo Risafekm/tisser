dcbcfd02bc9997b0ea56d3c0c1d1ddfc43cf0556
RISAF KM <101542049+Risafekm@users.noreply.github.com>
2025-08-27 10:19:42 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/tisser_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tisser_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: DashBoard());
+  }
+}
+
+class DashBoard extends StatelessWidget {
+  const DashBoard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
